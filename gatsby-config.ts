c1754cc6b749f3937bcc45e21f60b97b37dd2711
malochak <konrad_malocha@hotmail.com>
2022-05-29 14:02:41 +0200

    init gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Mkon`,
+    siteUrl: `https://www.mkon.dev`,
+  },
+  plugins: [],
+}
+
+export default config
